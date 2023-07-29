6ed1113ea1e7e292464c6b87903876e50c926e5f
vaibhav rehapade <vaibhav.rehapade@gmail.com>
2023-07-30 02:50:41 +0530

    Initial commit

---
+++ JunitAndMockito/src/test/java/com/dynamics/pro/JunitAndMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynamics.pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitAndMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
